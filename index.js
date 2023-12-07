e79f9a7d3aef8aae1cc3f9785ad24cff63edec57
CamilleAntonios <camille.antonios@gmail.com>
2023-12-07 22:08:43 +0100

    feat: Modification regarding the index page

---
+++ index.js
@@ -0,0 +1,9 @@
+let pendu = document.getElementById("pendu");
+let memory = document.getElementById("memory");
+let jeuPoisson = document.getElementById("jeuPoisson");
+let cliquerNettoyer = document.getElementById("cliquerNettoyer");
+
+pendu.addEventListener("click",()=> window.location.href="https://camilleantonios.github.io/pendu.html"); 
+memory.addEventListener("click", ()=> window.location.href="https://camilleantonios.github.io/memory.html");
+jeuPoisson.addEventListener("click", ()=> window.location.href="https://camilleantonios.github.io/jeuPoisson.html");
+cliquerNettoyer.addEventListener("click", ()=> window.location.href="https://camilleantonios.github.io/clic.html");
